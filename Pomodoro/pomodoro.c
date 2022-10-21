90cd257672561a8bb4a5cad4e654304e2c9a7f3e
JuanJakobo <github@johannsn.com>
2022-10-21 22:27:57 +0200

    Save config and current status in file

--- Pomodoro/pomodoro.c
+++ Pomodoro/pomodoro.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <notification/notification.h>
 #include <notification/notification_messages.h>
+#include <flipper_format/flipper_format.h>
 
 //Decide on the task to be done.
 //Set the pomodoro timer (typically for 25 minutes).[1]
@@ -13,15 +14,38 @@
 //After three pomodoros are done, take the fourth pomodoro and then take a long break (typically 20 to 30 minutes). Once the long break is finished, return to step 2.
 //https://en.wikipedia.org/wiki/Pomodoro_Technique
 
+#define CONFIG_FILE_DIRECTORY_PATH "/ext/apps/misc"
+#define CONFIG_FILE_PATH CONFIG_FILE_DIRECTORY_PATH "/pomodoro.conf"
 
-//TODO check memory
+#define CONFIG_FILE_HEADER "Flipper Pomodoro plugin config file"
+#define CONFIG_FILE_ACTUAL_VERSION 1
+#define POMODORO_CONFIG_KEY_WORK_TIME "workTime"
+#define POMODORO_CONFIG_KEY_SHORT_BREAK_TIME "shortBreakTime"
+#define POMODORO_CONFIG_KEY_LONG_BREAK_TIME "longBreakTime"
+#define POMODORO_CONFIG_KEY_COUNT "count"
+#define POMODORO_CONFIG_KEY_REPETITIONS "repetitions"
+#define POMODORO_CONFIG_KEY_STATE "state"
+
+// Default values
+#define WORK_TIME 25
+#define SHORT_BREAK_TIME 5
+#define LONG_BREAK_TIME 30
+
+typedef enum {
+    workTime = 0,
+    shortBreakTime = 1,
+    longBreakTime = 2,
+} PomodoroState;
+
+//TODO use other int?
 typedef struct {
-    uint8_t count;
-    uint8_t *endTime;
-    uint8_t workTime;
-    uint8_t shortBreakTime;
-    uint8_t longBreakTime;
-    uint8_t repetitions;
+    uint32_t count;
+    uint32_t *endTime;
+    uint32_t workTime;
+    uint32_t shortBreakTime;
+    uint32_t longBreakTime;
+    uint32_t repetitions;
+    PomodoroState state;
     bool running;
     bool notification;
 } Pomodoro;
@@ -122,15 +146,165 @@ static void pomodoro_stop_notification(Pomodoro *pomodoro){
     }
 }
 
+//File handling
+static Storage* pomodoro_open_storage() {
+    return furi_record_open(RECORD_STORAGE);
+}
+
+static void pomodoro_close_storage() {
+    furi_record_close(RECORD_STORAGE);
+}
+
+static void pomodoro_close_config_file(FlipperFormat* file) {
+    if(file == NULL) return;
+    flipper_format_file_close(file);
+    flipper_format_free(file);
+}
+
+static FlipperFormat* pomodoro_open_config_file(Storage* storage) {
+    FlipperFormat* format = flipper_format_file_alloc(storage);
+
+    if(storage_common_stat(storage, CONFIG_FILE_PATH, NULL) == FSE_OK) {
+        if(!flipper_format_file_open_existing(format, CONFIG_FILE_PATH)) {
+            pomodoro_close_config_file(format);
+            return NULL;
+        }
+    } else {
+        if(storage_common_stat(storage, CONFIG_FILE_DIRECTORY_PATH, NULL) == FSE_NOT_EXIST) {
+            if(!storage_simply_mkdir(storage, CONFIG_FILE_DIRECTORY_PATH)) {
+                return NULL;
+            }
+        }
+
+        if(!flipper_format_file_open_new(format, CONFIG_FILE_PATH)) {
+            pomodoro_close_config_file(format);
+            return NULL;
+        }
+
+        //Default Values
+        flipper_format_write_header_cstr(format, CONFIG_FILE_HEADER, CONFIG_FILE_ACTUAL_VERSION);
+        flipper_format_write_comment_cstr(format, " ");
+
+        const uint32_t workTime = WORK_TIME;
+        flipper_format_write_comment_cstr(format, "Time of a Pomodoro");
+        flipper_format_write_uint32(format, POMODORO_CONFIG_KEY_WORK_TIME, &workTime, 1);
+
+        uint32_t shortBreakTime = SHORT_BREAK_TIME;
+        flipper_format_write_comment_cstr(format, "Time of a short Break");
+        flipper_format_write_uint32(format, POMODORO_CONFIG_KEY_SHORT_BREAK_TIME, &shortBreakTime, 1);
+
+        uint32_t longBreakTime = LONG_BREAK_TIME;
+        flipper_format_write_comment_cstr( format, "Time of a long Break");
+        flipper_format_write_uint32(format, POMODORO_CONFIG_KEY_LONG_BREAK_TIME, &longBreakTime, 1);
+
+        uint32_t defaultValue = 0;
+        flipper_format_write_comment_cstr(format, " ");
+        flipper_format_write_comment_cstr(format, "Values for current run.");
+        flipper_format_write_comment_cstr( format, "Minutes in current state");
+        flipper_format_write_uint32(format, POMODORO_CONFIG_KEY_COUNT, &defaultValue, 1);
+        flipper_format_write_comment_cstr( format, "Repititions in current run");
+        flipper_format_write_uint32(format, POMODORO_CONFIG_KEY_REPETITIONS, &defaultValue, 1);
+        flipper_format_write_comment_cstr( format, "Current state(0 = work, 1 = short Break, 2 = long Break");
+        flipper_format_write_uint32(format, POMODORO_CONFIG_KEY_STATE, &defaultValue, 1);
+
+    }
+
+    if(!flipper_format_rewind(format)) {
+        pomodoro_close_config_file(format);
+        return NULL;
+    }
+
+    return format;
+}
+
+
+static void pomodoro_save_current_run(Pomodoro * const pomodoro) {
+    Storage* cfg_storage = pomodoro_open_storage();
+    FlipperFormat* file = pomodoro_open_config_file(cfg_storage);
+
+    flipper_format_insert_or_update_uint32(file, POMODORO_CONFIG_KEY_COUNT, &pomodoro->count, 1);
+    flipper_format_insert_or_update_uint32(file, POMODORO_CONFIG_KEY_REPETITIONS, &pomodoro->repetitions, 1);
+    uint32_t state = pomodoro->state;
+    flipper_format_insert_or_update_uint32(file, POMODORO_CONFIG_KEY_STATE, &state, 1);
+
+    pomodoro_close_config_file(file);
+    pomodoro_close_storage();
+}
+
+static void pomodoro_save_settings(Pomodoro * const pomodoro) {
+    Storage* cfg_storage = pomodoro_open_storage();
+    FlipperFormat* file = pomodoro_open_config_file(cfg_storage);
+
+    flipper_format_insert_or_update_uint32(file, POMODORO_CONFIG_KEY_WORK_TIME, &pomodoro->workTime, 1);
+    flipper_format_insert_or_update_uint32(file, POMODORO_CONFIG_KEY_SHORT_BREAK_TIME, &pomodoro->shortBreakTime, 1);
+    flipper_format_insert_or_update_uint32(file, POMODORO_CONFIG_KEY_LONG_BREAK_TIME, &pomodoro->longBreakTime, 1);
+
+    pomodoro_close_config_file(file);
+    pomodoro_close_storage();
+}
+
+
 static void pomodoro_init(Pomodoro* const pomodoro) {
-    //TODO change to minutes
-    pomodoro->count = 0;
-    pomodoro->workTime = 25;
-    pomodoro->shortBreakTime = 5;
-    pomodoro->longBreakTime = 30;
-    pomodoro->endTime= &pomodoro->workTime;
-    pomodoro->repetitions = 0;
-    pomodoro->running = false;
+
+    Storage* storage = pomodoro_open_storage();
+    FlipperFormat* format = pomodoro_open_config_file(storage);
+
+    FuriString* file_type = furi_string_alloc();
+
+    //TODO use other int
+    uint32_t version = 1;
+    if(!flipper_format_read_header(format, file_type, &version)) {
+        furi_string_free(file_type);
+        return;
+    }
+
+    flipper_format_rewind(format);
+
+    //TODO set default values once
+    if(!flipper_format_read_uint32( format, POMODORO_CONFIG_KEY_WORK_TIME, &pomodoro->workTime, 1)){
+        pomodoro->workTime = WORK_TIME;
+    }
+
+    if(!flipper_format_read_uint32( format, POMODORO_CONFIG_KEY_SHORT_BREAK_TIME, &pomodoro->shortBreakTime, 1)) {
+        pomodoro->shortBreakTime = SHORT_BREAK_TIME;
+    }
+
+    if(!flipper_format_read_uint32( format, POMODORO_CONFIG_KEY_LONG_BREAK_TIME, &pomodoro->longBreakTime, 1)) {
+        pomodoro->longBreakTime = LONG_BREAK_TIME;
+    }
+
+    if(!flipper_format_read_uint32( format, POMODORO_CONFIG_KEY_COUNT, &pomodoro->count, 1)) {
+        pomodoro->count = 0;
+    }
+
+    if(!flipper_format_read_uint32( format, POMODORO_CONFIG_KEY_REPETITIONS, &pomodoro->repetitions, 1)) {
+        pomodoro->repetitions = 0;
+    }
+
+    pomodoro->running = (pomodoro->count > 0 || pomodoro->repetitions > 0) ? true : false;
+
+    uint32_t state;
+    if(!flipper_format_read_uint32( format, POMODORO_CONFIG_KEY_STATE, &state, 1)) {
+        pomodoro->state = 0;
+    }
+
+    pomodoro->state = state;
+    switch(pomodoro->state){
+        case longBreakTime:
+            pomodoro->endTime = &pomodoro->longBreakTime;
+            break;
+        case shortBreakTime:
+            pomodoro->endTime = &pomodoro->shortBreakTime;
+            break;
+        default:
+            pomodoro->endTime = &pomodoro->workTime;
+            break;
+    }
+
+    furi_string_free(file_type);
+    pomodoro_close_config_file(format);
+    pomodoro_close_storage();
+
 }
 
 int32_t pomodoro_app(void* p) {
@@ -152,6 +326,7 @@ int32_t pomodoro_app(void* p) {
     view_port_draw_callback_set(view_port, draw_callback, &state_mutex);
     view_port_input_callback_set(view_port, input_callback, event_queue);
 
+    //start timer when is running is true?
     FuriTimer* timer = furi_timer_alloc(pomodoro_update_timer_callback, FuriTimerTypePeriodic, event_queue);
     furi_timer_start(timer, furi_ms_to_ticks(1000));
     uint8_t tick = 0;
@@ -181,14 +356,14 @@ int32_t pomodoro_app(void* p) {
                             else if(pomodoro->notification)
                                 pomodoro_stop_notification(pomodoro);
                             break;
-                        //Select next choosen object, if start false
+                            //Select next choosen object, if start false
                         case InputKeyDown:
                             if(!pomodoro->running && (*pomodoro->endTime -1) > 0)
                                 (*pomodoro->endTime)--;
                             else if(pomodoro->notification)
                                 pomodoro_stop_notification(pomodoro);
                             break;
-                        //Increase timer for choosen object, if start false
+                            //Increase timer for choosen object, if start false
                         case InputKeyRight:
                             //TODO use -- and ++?
                             if(!pomodoro->running){
@@ -217,6 +392,7 @@ int32_t pomodoro_app(void* p) {
                         case InputKeyBack:
                             if(pomodoro->running){
                                 pomodoro->endTime = &pomodoro->workTime;
+                                pomodoro->state = workTime;
                                 pomodoro->count = 0;
                                 pomodoro->repetitions = 0;
                                 tick = 0;
